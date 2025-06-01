7a5f1461f785a3728c327634d1ec54ca26674a9c
Sung Yang Cheung <zhang.shengyang@outlook.com>
2025-06-01 18:11:42 +0800

    Site updated: 2025-06-01 18:11:42

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/28/OpenAI的Prompt Engineering 课程 1/","2024/05/24/hello-world/","2025/05/30/OpenAI的Prompt Engineering 课程 2/","2025/06/01/OpenAI的Prompt Engineering 课程 4/","2025/05/31/OpenAI的Prompt Engineering 课程 3/"];function toRandomPost(){
+var posts=["2025/05/28/OpenAI的Prompt Engineering 课程 1/","2025/05/30/OpenAI的Prompt Engineering 课程 2/","2025/05/31/OpenAI的Prompt Engineering 课程 3/","2024/05/24/hello-world/","2025/06/01/OpenAI的Prompt Engineering 课程 4/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
